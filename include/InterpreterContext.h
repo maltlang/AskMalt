cce0f9ef5641ec9d57fe1a6117fc29b498846086
Zhihang-Liu <enterindex@gmail.com>
2018-08-26 08:01:26 +0800

    small update

--- include/InterpreterContext.h
+++ include/InterpreterContext.h
@@ -1,8 +1,19 @@
 #pragma once
+#include "Parser/AST.h"
+#include "Function.h"
+#include "FunctionContext.h"
+#include <stack>
 
 namespace amalt {
 	class InterpreterContext {
 	public:
+		String path;
+		std::unordered_map<String, Module> modtable;
+		std::stack<FunctionContext> framestack;
 
+		InterpreterContext() = default;
+		InterpreterContext(int argc, char **argv);
+		InterpreterContext() = default;
+		friend Value expr(AST a);
 	};
 }
